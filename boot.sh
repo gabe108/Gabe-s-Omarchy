1e2268c82b9ad93177f05731b1e8b8abfe0f4ef0
Muhammad Ghalib <36465177+gabe108@users.noreply.github.com>
2025-08-23 00:05:17 +1000

    updated boot with this repo address

--- boot.sh
+++ boot.sh
@@ -16,8 +16,8 @@ echo -e "\n$ansi_art\n"
 
 sudo pacman -Sy --noconfirm --needed git
 
-# Use custom repo if specified, otherwise default to basecamp/omarchy
-OMARCHY_REPO="${OMARCHY_REPO:-basecamp/omarchy}"
+# Use custom repo if specified, otherwise default to gabe108/Gabe-s-Omarchy
+OMARCHY_REPO="${OMARCHY_REPO:-gabe108/Gabe-s-Omarchy/}"
 
 echo -e "\nCloning Omarchy from: https://github.com/${OMARCHY_REPO}.git"
 rm -rf ~/.local/share/omarchy/
